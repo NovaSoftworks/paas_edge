cef477851899e86c7bf4a217bc4056742c9eb141
marclamy <marc.lamy83@gmail.com>
2023-12-24 04:16:14 +0100

    mark the root module's existing outputs as sensitive

--- environments/dev/output.tf
+++ environments/dev/output.tf
@@ -1,14 +1,17 @@
 output "k8s" {
   value       = module.k8s.k8s
   description = "The Kubernetes cluster."
+  sensitive   = true
 }
 
 output "mongo" {
   value       = module.mongo.mongo
   description = "The MongoDB account."
+  sensitive   = true
 }
 
 output "postgres" {
   value       = module.postgres.postgres
   description = "The PostgreSQL server."
+  sensitive   = true
 }
