7fde50dfb36587e6c16467e9d51027c3eee18653
marclamy <marc.lamy83@gmail.com>
2023-12-24 21:56:11 +0100

    split the nexus k8s namespace in frontend and backend

--- stage2/modules/k8s/main.tf
+++ stage2/modules/k8s/main.tf
@@ -1,5 +1,11 @@
-resource "kubernetes_namespace" "k8s_namespace" {
+resource "kubernetes_namespace" "k8_frontend_namespace" {
   metadata {
-    name = "nexus"
+    name = "frontend"
+  }
+}
+
+resource "kubernetes_namespace" "k8s_backend_namespace" {
+  metadata {
+    name = "backend"
   }
 }
