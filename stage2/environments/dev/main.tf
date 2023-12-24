1168e5f1f487a760708991cdfc8d2a5ce1b6debf
marclamy <marc.lamy83@gmail.com>
2023-12-24 16:13:53 +0100

    create the Kubernetes workspace for Nexus

---
+++ stage2/environments/dev/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24"
+    }
+  }
+  cloud {
+    organization = "NovaSoftworks"
+    workspaces {
+      name = "nexus-infrastructure-stage2-dev"
+    }
+  }
+}
+
+data "terraform_remote_state" "stage1" {
+  backend = "remote"
+
+  config = {
+    organization = "NovaSoftworks"
+    workspaces = {
+      name = "nexus-infrastructure-stage1-dev"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  kube_config = data.terraform_remote_state.stage1.outputs.k8s.kube_config.0
+}
+
+provider "kubernetes" {
+  host                   = local.kube_config.host
+  client_certificate     = base64decode(local.kube_config.client_certificate)
+  client_key             = base64decode(local.kube_config.client_key)
+  cluster_ca_certificate = base64decode(local.kube_config.cluster_ca_certificate)
+}
+
+module "k8s" {
+  source = "../../modules/k8s"
+}
