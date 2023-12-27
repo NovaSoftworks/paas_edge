fff1c6e65e3605e06407d5610bc992d3602ae0e3
marclamy <marc.lamy83@gmail.com>
2023-12-27 01:04:55 +0100

    create an auth k8s namespace

--- stage2/modules/k8s/main.tf
+++ stage2/modules/k8s/main.tf
@@ -1,3 +1,9 @@
+resource "kubernetes_namespace" "k8_auth_namespace" {
+  metadata {
+    name = "auth"
+  }
+}
+
 resource "kubernetes_namespace" "k8_frontend_namespace" {
   metadata {
     name = "frontend"
