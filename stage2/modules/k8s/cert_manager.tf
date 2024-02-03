6fd274e5de09c09f5ae7dd3108b198da3dddb705
Marc LAMY <marc.lamy83@gmail.com>
2024-02-03 16:45:17 +0100

    complete the vert manager configuration for k8s

--- stage2/modules/k8s/cert_manager.tf
+++ stage2/modules/k8s/cert_manager.tf
@@ -12,9 +12,14 @@ resource "helm_release" "k8s_cert_manager" {
   namespace  = kubernetes_namespace.cert_manager.metadata.0.name
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
+  version    = "v1.13.3"
 
   set {
     name  = "installCRDs"
     value = "true"
   }
+
+  depends_on = [
+    kubernetes_namespace.cert_manager
+  ]
 }
