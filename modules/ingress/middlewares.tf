d1bac3285a99e1725116e077f3e7cc2c1532c0a1
marclamy <marc.lamy83@gmail.com>
2024-02-26 20:55:51 +0100

    update traefik apiVersion

--- modules/ingress/middlewares.tf
+++ modules/ingress/middlewares.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_manifest" "ingress_middleware_redirect_to_https" {
   manifest = {
-    apiVersion = "traefik.containo.us/v1alpha1"
+    apiVersion = "traefik.io/v1alpha1"
     kind       = "Middleware"
     metadata = {
       name      = "redirect-to-https"
@@ -17,7 +17,7 @@ resource "kubernetes_manifest" "ingress_middleware_redirect_to_https" {
 
 resource "kubernetes_manifest" "ingress_middleware_redirect_to_non_www" {
   manifest = {
-    apiVersion = "traefik.containo.us/v1alpha1"
+    apiVersion = "traefik.io/v1alpha1"
     kind       = "Middleware"
     metadata = {
       name      = "redirect-to-non-www"
